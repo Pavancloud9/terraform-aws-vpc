1f73c25226f75dcd0ab083c628f52e5c4dd2aa4d
pavan <pavankumar49210@gmail.com>
2025-12-23 22:44:25 +0530

    added

--- outputs.tf
+++ outputs.tf
@@ -10,3 +10,21 @@ output "default_vpc_route_table_id" {
   value = data.aws_route_table.main.id
 }
 
+output "vpc_id" {
+  value  = aws_vpc.main.id 
+}
+
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+output "database_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+
+
